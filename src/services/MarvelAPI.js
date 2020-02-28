5446255be24a2b6a826df5512c2707bff5bb4fe4
Gabriel Cunha <gn_cunha@hotmail.com>
2020-02-27 21:02:49 -0300

    correções na requisições da api/resolvendo erro de nao autorizado

--- src/services/MarvelAPI.js
+++ src/services/MarvelAPI.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
-const URL_BASE_MARVEL = 'https://gateway.marvel.com:443/v1/public/'
-const API_KEY = 'YOUR-API-KEY'
+const URL_BASE_MARVEL = 'https://gateway.marvel.com/v1/public/'
+const API_KEY = 'sua chave publica aqui'
+const TS = 'timestamp'
+const HASH = 'gerar um md5(Timestamp+privateKey+publicKey)'
 
 export default {
   getAllCharacteres: (limit, callback) => {
-    const URL_CHARACTERS = `${URL_BASE_MARVEL}characters?apikey=${API_KEY}&limit=${limit}`
+    const URL_CHARACTERS = `${URL_BASE_MARVEL}characters?ts=${TS}&apikey=${API_KEY}&hash=${HASH}&limit=${limit}`
     axios.get(URL_CHARACTERS).then(characters => {
       if (callback) {
         callback(characters)
@@ -13,7 +15,7 @@ export default {
     })
   },
   getCharacterByID: (characterID, callback) => {
-    const URL_CHARACTER_BY_ID = `${URL_BASE_MARVEL}characters/${characterID}?apikey=${API_KEY}`
+    const URL_CHARACTER_BY_ID = `${URL_BASE_MARVEL}characters/${characterID}?apikey=${API_KEY}&ts=${TS}&hash=${HASH}`
     axios.get(URL_CHARACTER_BY_ID).then(character => {
       if (callback) {
         callback(character)
@@ -21,7 +23,7 @@ export default {
     })
   },
   searchCharacter: (name, callback) => {
-    const URL_SEARCH_CHARACTER = `${URL_BASE_MARVEL}characters?apikey=${API_KEY}&name=${name}`
+    const URL_SEARCH_CHARACTER = `${URL_BASE_MARVEL}characters?ts=${TS}&apikey=${API_KEY}&hash=${HASH}&name=${name}`
     axios.get(URL_SEARCH_CHARACTER).then(character => {
       if (callback) {
         callback(character)
@@ -29,3 +31,4 @@ export default {
     })
   }
 }
+
